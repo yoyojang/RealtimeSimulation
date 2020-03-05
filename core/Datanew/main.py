b5f107dd0cdd031bb47cffbc11c8db745acca8a1
510588264@qq.com <510588264@qq.com>
2020-03-05 20:22:43 +0800

    20200305 评价数据处理

--- core/Datanew/main.py
+++ core/Datanew/main.py
@@ -1,5 +1,41 @@
+import os
+import time
+from conf.config import *
 
-# 提取数据库中rate Volume list里的文件
-# rate 链接数据库替换
-# 定时更新至route_rate volume
-# 在主程序之前运行
+def ReadTargetFile(path,hour):
+    '''
+    获取目标文件，返回生成器
+    :param path: 历史流量文件夹
+    :param hour: 当前小时数
+    :return: 数据对应生成器
+    '''
+    filename = os.path.join(path, '{}.txt'.format(hour))
+    with open(filename,encoding='utf-8') as f:
+        while 1:
+            l = f.readline()
+            if l:
+                yield l
+            else:
+                break
+
+def WriteRealtimeFile(filename, content):
+    '''
+    将历史流量输入到缓存文件中
+    :param filename: 缓存文件名称
+    :param content: 输入内容
+    :return: 无
+    '''
+    with open(filename,'w',encoding='utf-8') as f:
+        for i in content:
+            f.write(i)
+    print('finish')
+
+while 1:
+    hour = time.localtime(time.time()).tm_hour
+    if hour < 24:
+        WriteRealtimeFile(volume_file,ReadTargetFile(VolumeHistory,hour))
+        WriteRealtimeFile(routerate_file,ReadTargetFile(RateHistory,hour))
+        print(hour)
+        time.sleep(10)
+    else:
+        break
--- core/EvaluationToDatabase/main.py
+++ core/EvaluationToDatabase/main.py
@@ -2,25 +2,72 @@
 # 与主程序同时运行，并在主程序结束后结束
 
 import pymysql
+import re
 
 from conf.config import *
 
 
 
 
-def main():
-    # 实时获取三个文件内容
-    # 如果有更新 则打开数据库 输入数据
-    # 关闭数据库
-
-def tail(filename):
+def listen(filename):
     with open(filename, encoding='utf-8') as f:
         while 1:
             line = f.readline()
             if line.strip():
                 yield line.strip()
 
+def handle_obj(each):
+    date = each[:19]
+    # 使用正则取出当前时间每个检测器数据
+    obj_group = re.findall('(\([^\)]*\))', each)
+    # print(x)
+    k_lst = []
+    for i in obj_group:
+        # 把每个检测器数据分割，并将字符串转化为数字
+        k = i[1:-1].strip().split(',')
+        # print(k)
+        for j in range(len(k)):
+            if 'None' in k[j]:
+                k[j] = None
+            elif '.' in k[j]:
+                k[j] = float(k[j])
+            else:
+                # print(k[j])
+                k[j] = int(k[j])
+        k.insert(0, date)
+        k = tuple(k)
+        k_lst.append(k)
+        return k_lst
 
-g = tail(datacollection_file)
-for i in g:
-    print(i)
+def handle_movement(each):
+    pass
+
+def main(file,sql):
+    listen_each = listen(file)
+    for each in listen_each:
+        k_lst = handle_obj(each)
+        try:
+            cursor.executemany(sql, k_lst)
+            conn.commit()
+        except:
+            print("无法插入流量")
+
+def main_movement(file,sql_edge,sql_node):
+    pass
+
+sql_trvtm = "insert into traveltime() values(%s,%s,%s,%s,%s)"
+sql_node = "insert into node_movement() values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+sql_edge = "insert into edge_movement() values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+sql_datacollection = "insert into datacollection() values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+# conn = pymysql.connect(host="localhost", user="root", password='123456', database='traffic_test',
+#                                    charset='utf8')
+# cursor = conn.cursor()
+conn = pymysql.connect(host="172.192.10.101", user="root", password='123456', database='trsp',charset='utf8')
+cursor = conn.cursor()
+
+main(datacollection_file,sql_datacollection)
+main(traveltime_file,sql_trvtm)
+# main(movement_file)
+
+conn.close()
